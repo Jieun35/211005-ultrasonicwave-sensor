51279baa55f10dd65f1b5b06fb1fea24dd188ce7
Jieun35 <90733575+Jieun35@users.noreply.github.com>
2021-10-05 11:05:44 +0900

    pxt.json, main.blocks, main.ts 업데이트하기

--- main.ts
+++ main.ts
@@ -1,16 +1,7 @@
-let cm = 0
 basic.forever(function () {
-    pins.digitalWritePin(DigitalPin.P1, 0)
-    basic.pause(2)
-    pins.digitalWritePin(DigitalPin.P1, 1)
-    basic.pause(10)
-    pins.digitalWritePin(DigitalPin.P1, 0)
-    cm = pins.map(
-    pins.pulseIn(DigitalPin.P2, PulseValue.High),
-    0,
-    46473,
-    2,
-    1000
-    )
-    basic.showNumber(cm)
+    basic.showNumber(sonar.ping(
+    DigitalPin.P1,
+    DigitalPin.P2,
+    PingUnit.Centimeters
+    ))
 })
